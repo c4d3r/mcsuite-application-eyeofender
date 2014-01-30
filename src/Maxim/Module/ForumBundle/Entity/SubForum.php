c200314e38ad3967d65515cfc6d602d5d54aafd7
Maxim <maximgeerinck@hotmail.com>
2014-01-30 22:31:11 +0100

    Add subforum entity

--- src/Maxim/Module/ForumBundle/Entity/SubForum.php
+++ src/Maxim/Module/ForumBundle/Entity/SubForum.php
@@ -1,14 +1,176 @@
 <?php
 /**
- * Author: Maxim
- * Date: 30/01/14
- * Time: 22:24
- * Property of MCSuite
+ * Created by IntelliJ IDEA.
+ * User: Maxim
+ * Date: 16/09/13
+ * Time: 18:51
+ * To change this template use File | Settings | File Templates.
  */
 
 namespace Maxim\Module\ForumBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
 
-class SubForum {
+class SubForum
+{
+    protected $id;
 
-} 
+    protected $forum;
+
+    protected $title;
+
+    protected $description;
+
+    protected $createdBy;
+
+    protected $createdOn;
+
+    protected $updatedBy;
+
+    protected $updatedOn;
+
+    protected $sort;
+
+    public function setCreatedBy($createdBy)
+    {
+        $this->createdBy = $createdBy;
+    }
+
+    /**
+     * @return \Maxim\Module\ForumBundle\Entity\User
+     */
+    public function getCreatedBy()
+    {
+        return $this->createdBy;
+    }
+
+    /**
+     * @param \Maxim\Module\ForumBundle\Entity\datetime $createdOn
+     */
+    public function setCreatedOn($createdOn)
+    {
+        $this->createdOn = $createdOn;
+    }
+
+    /**
+     * @return \Maxim\Module\ForumBundle\Entity\datetime
+     */
+    public function getCreatedOn()
+    {
+        return $this->createdOn;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param \Maxim\Module\ForumBundle\Entity\Forum $forum
+     */
+    public function setForum($forum)
+    {
+        $this->forum = $forum;
+    }
+
+    /**
+     * @return \Maxim\Module\ForumBundle\Entity\Forum
+     */
+    public function getForum()
+    {
+        return $this->forum;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $sort
+     */
+    public function setSort($sort)
+    {
+        $this->sort = $sort;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSort()
+    {
+        return $this->sort;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param \Maxim\Module\ForumBundle\Entity\User $updatedBy
+     */
+    public function setUpdatedBy($updatedBy)
+    {
+        $this->updatedBy = $updatedBy;
+    }
+
+    /**
+     * @return \Maxim\Module\ForumBundle\Entity\User
+     */
+    public function getUpdatedBy()
+    {
+        return $this->updatedBy;
+    }
+
+    /**
+     * @param \Maxim\Module\ForumBundle\Entity\datetime $updatedOn
+     */
+    public function setUpdatedOn($updatedOn)
+    {
+        $this->updatedOn = $updatedOn;
+    }
+
+    /**
+     * @return \Maxim\Module\ForumBundle\Entity\datetime
+     */
+    public function getUpdatedOn()
+    {
+        return $this->updatedOn;
+    }
+
+
+}
