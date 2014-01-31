2d8ecaede357df7f8ef563b60ce7ed4875ddb56c
Maxim <maximgeerinck@hotmail.com>
2014-01-31 14:28:50 +0100

    Fix mappings in item en category Add mappings for payum

--- src/Maxim/CMSBundle/Entity/PayumSecurityToken.php
+++ src/Maxim/CMSBundle/Entity/PayumSecurityToken.php
@@ -5,10 +5,6 @@ namespace Maxim\CMSBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
 use Payum\Core\Model\Token;
 
-/**
- * @ORM\Table(name="payum_security_token")
- * @ORM\Entity
- */
 class PayumSecurityToken extends Token
 {
 
